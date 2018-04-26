77c64f65e2930ab793f17afb69f7582a7ccbe941
Alex H. Wagner, PhD <awagner24@wustl.edu>
2018-04-26 00:55:41 -0500

    initial model commit

---
+++ pycivic/model.py
@@ -0,0 +1,43 @@
+import requests
+from collections import OrderedDict
+
+
+class CivicRecord:
+
+    def __init__(self, **kwargs):
+        self.id = kwargs['id']
+        self.type = kwargs['type']
+
+class Assertion(CivicRecord):
+
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        # TODO: other Assertion fields
+
+
+class CivicDb:
+
+    API_URL = 'https://civicdb.org/api'
+
+    def __init__(self):
+        self._assertions = dict()
+        self._variants = dict()
+        self._evidence = dict()
+
+    def get_assertions(self, assertion_id_list, update=False):
+        out = OrderedDict()
+        for assertion_id in assertion_id_list:
+            out[assertion_id] = self.get_assertion(assertion_id, update)
+        return out
+
+    def get_assertion(self, assertion_id, update=False):
+        assertion_id_int = int(assertion_id)
+        if not self._assertions.get(assertion_id_int, False) or update:
+            url = '/'.join(CivicDb.API_URL, assertion_id_int)
+            resp = requests.get(url)
+            resp.raise_for_status()
+            self._assertions[assertion_id_int] = Assertion(resp.json())
+        return self._assertions[assertion_id_int]
+
+    def get_all_assertions(self):
+        raise NotImplementedError
