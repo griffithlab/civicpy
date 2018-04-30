c8ec2ecd7ccfe25cec090bad96b6cbbbbf1c0922
Alex H. Wagner, PhD <awagner24@wustl.edu>
2018-04-30 00:39:36 -0500

    init setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pycivic',
+    version='0.0.1.alpha',
+    packages=['pycivic'],
+    url='https://github.com/griffithlab/pycivic',
+    license='MIT',
+    author='Alex H. Wagner',
+    author_email='awagner24@wustl.edu',
+    description='a python wrapper and tools for the civic API'
+)
