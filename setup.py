c3446e78523f2cc8e5ea7f5ec458d08756ca8fe6
Alex H. Wagner, PhD <awagner24@wustl.edu>
2018-04-30 23:48:08 -0500

    update for packaging

--- setup.py
+++ setup.py
@@ -2,11 +2,26 @@ from setuptools import setup
 
 setup(
     name='civicpy',
-    version='0.0.1.alpha',
+    version='0.0.2.a1',
     packages=['civicpy'],
     url='https://github.com/griffithlab/civicpy',
     license='MIT',
     author='Alex H. Wagner',
     author_email='awagner24@wustl.edu',
-    description='a python wrapper and tools for the civic API'
+    description='a python wrapper and tools for the civic API',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=[
+        'pytest>=3.5',
+        'requests>=2.18'
+    ],
+    python_requires='~=3.6',
+    entry_points={},
+
 )
