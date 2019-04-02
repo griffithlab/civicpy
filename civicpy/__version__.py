feac435483bac46ea650f46d1b4f15eb3395a2b8
Alex H. Wagner, PhD <awagner24@wustl.edu>
2019-04-01 21:53:50 -0400

    post fix
    fix setup file

--- civicpy/__version__.py
+++ civicpy/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'civicpy'
 __description__ = 'CIViC variant knowledgebase analysis toolkit.'
 __url__ = 'http://civicpy.org'
-__version__ = '0.0.3'
+__version__ = '0.0.3.post1'
 # __build__ = 0x021901
 __author__ = 'Alex H. Wagner'
 __author_email__ = 'ahwagner22@gmail.com'
