1753292e939331fe93d4653e1c226fe210ac0462
Alex H. Wagner, PhD <awagner24@wustl.edu>
2019-04-01 21:30:44 -0400

    add analysis from AACR

--- civicpy/__version__.py
+++ civicpy/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'civicpy'
 __description__ = 'CIViC variant knowledgebase analysis toolkit.'
 __url__ = 'http://civicpy.org'
-__version__ = '0.0.3a1'
+__version__ = '0.0.3'
 # __build__ = 0x021901
 __author__ = 'Alex H. Wagner'
 __author_email__ = 'ahwagner22@gmail.com'
