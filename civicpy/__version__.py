e27fc12b656f3706bb7bc7f0142970342448389d
Alex H. Wagner, PhD <awagner24@wustl.edu>
2019-12-20 22:25:06 -0600

    version update, and a happy new year

--- civicpy/__version__.py
+++ civicpy/__version__.py
@@ -4,7 +4,7 @@ __url__ = 'http://civicpy.org'
 __major__ = '1'
 __minor__ = '0'
 __patch__ = '0'
-__meta_label__ = 'rc3'
+__meta_label__ = 'rc4'
 __short_version__ = f"{__major__}.{__minor__}"
 __version__ = f"{__short_version__}.{__patch__}"
 if __meta_label__:
@@ -12,4 +12,4 @@ if __meta_label__:
 __authors__ = ['Alex H. Wagner', 'Susanna Kiwala']
 __author_email__ = 'help@civicpy.org'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2018-2019 The Griffith Lab'
+__copyright__ = 'Copyright 2018-2020 The Griffith Lab'
