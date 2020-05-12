f6cc8f23da4270b42bda7f4953860350ff96a9f5
Susanna Kiwala <susanna.kiwala@wustl.edu>
2020-05-12 12:10:48 -0500

    Add command to annotate an existing VCF with CIViC variants

--- civicpy/civic.py
+++ civicpy/civic.py
@@ -590,12 +590,14 @@ class Variant(CivicRecord):
         else:
             return ''
 
-    def csq(self):
+    def csq(self, include_status=None):
         if self.csq_alt() is None:
             return []
         else:
             csq = []
             for evidence in self.evidence:
+                if include_status is not None and evidence.status not in include_status:
+                    continue
                 special_character_table = str.maketrans(VCFWriter.SPECIAL_CHARACTERS)
                 csq.append('|'.join([
                     self.csq_alt(),
@@ -621,6 +623,8 @@ class Variant(CivicRecord):
                     evidence.status,
                 ]))
             for assertion in self.assertions:
+                if include_status is not None and assertion.status not in include_status:
+                    continue
                 csq.append('|'.join([
                     self.csq_alt(),
                     '&'.join(map(lambda t: t.name, self.variant_types)),
