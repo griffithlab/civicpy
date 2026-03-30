95c7db44debb73ee4523cdd21aaffea88db9c259
Susanna Kiwala <susanna.kiwala@wustl.edu>
2026-03-30 10:53:37 -0500

    Fix reference build formatting in fusion coordinates

--- civicpy/civic.py
+++ civicpy/civic.py
@@ -2226,6 +2226,19 @@ def _postprocess_response_element(e, element):
         elif e["__typename"] == "FactorVariant":
             e["subtype"] = "factor_variant"
         elif e["__typename"] == "FusionVariant":
+            for c in [
+                e["five_prime_coordinates"],
+                e["three_prime_coordinates"],
+                e["five_prime_start_exon_coordinates"],
+                e["five_prime_end_exon_coordinates"],
+                e["three_prime_start_exon_coordinates"],
+                e["three_prime_end_exon_coordinates"]
+            ]:
+                if c:
+                    if c["reference_build"] == "GRCH37":
+                        c["reference_build"] = "GRCh37"
+                    elif c["reference_build"] == "GRCH38":
+                        c["reference_build"] = "GRCh38"
             if (
                 e["five_prime_start_exon_coordinates"]
                 and e["five_prime_start_exon_coordinates"]["exon_offset"] is None
