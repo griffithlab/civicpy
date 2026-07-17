dcfc0f9fa8644e659b23a71e3435d3d76538b552
Kori Kuzma <korikuzma@gmail.com>
2026-07-17 09:56:33 -0400

    fix!: remove temp citations ext, use reportedIn instead (#246)
    close #219
    
    * This removes 'citations' extension from statements, and instead stores these in the `reportedIn` field
    * Use documents where possible, otherwise use IRIs

--- civicpy/exports/civic_gks_record.py
+++ civicpy/exports/civic_gks_record.py
@@ -15,6 +15,7 @@ from ga4gh.core.models import (
     ConceptMapping,
     Extension,
     MappableConcept,
+    MembershipOperator,
     Relation,
     iriReference,
 )
@@ -34,7 +35,6 @@ from ga4gh.va_spec.base import (
     DiagnosticPredicate,
     Direction,
     Document,
-    MembershipOperator,
     Method,
     PrognosticPredicate,
     Statement,
@@ -835,22 +835,24 @@ class CivicGksSource(Document):
     :param source: CIViC source record
     """
 
-    def __init__(self, source: Source) -> None:
+    def __init__(self, source: Source, urls: list[str] | None = None) -> None:
         """Initialize CivicGksSource class
 
         :param source: CIViC source record
+        :param urls: List of additional URLs to include in the document
         """
-        urls = [f"{LINKS_URL}/source/{source.id}", source.source_url]
+        source_urls = urls or []
+        source_urls.extend([f"{LINKS_URL}/source/{source.id}", source.source_url])
         pmid = source.citation_id if source.source_type == "PUBMED" else None
         if pmc_id := source.pmc_id:
-            urls.append(f"https://www.ncbi.nlm.nih.gov/pmc/articles/{pmc_id}")
+            source_urls.append(f"https://www.ncbi.nlm.nih.gov/pmc/articles/{pmc_id}")
 
         super().__init__(
             id=f"civic.sid:{source.id}",
             name=source.citation,
             title=source.title,
             pmid=pmid,
-            urls=urls,
+            urls=source_urls,
         )
 
 
@@ -911,8 +913,10 @@ class CivicGksEvidence(Statement, _CivicGksEvidenceAssertionMixin):
                 CivicEvidenceLevel(evidence_item.evidence_level)
             ),
             reportedIn=[
-                CivicGksSource(evidence_item.source),
-                iriReference(f"{LINKS_URL}/evidence/{evidence_item.id}"),
+                CivicGksSource(
+                    evidence_item.source,
+                    urls=[f"{LINKS_URL}/evidence/{evidence_item.id}"],
+                ),
             ],
         )
 
@@ -948,13 +952,22 @@ class _CivicGksAssertionMixin:
         ]
 
     @staticmethod
-    def get_reported_in(assertion: Assertion) -> list[iriReference]:
+    def get_reported_in(assertion: Assertion) -> list[iriReference | Document]:
         """Get reported in information for an assertion
 
         :param assertion: CIViC assertion record
         :return: List of CIViC links to records which the assertion is reported in
         """
-        return [iriReference(f"{LINKS_URL}/assertion/{assertion.id}")]
+        reported_in: list[iriReference | Document] = [
+            iriReference(f"{LINKS_URL}/assertion/{assertion.id}")
+        ]
+        for evidence_item in assertion.evidence_items or []:
+            civic_gks_source = CivicGksSource(
+                evidence_item.source,
+                urls=[f"{LINKS_URL}/evidence/{evidence_item.id}"],
+            )
+            reported_in.append(Document.model_validate(civic_gks_source))
+        return reported_in
 
 
 class CivicGksClinSigAssertion(
@@ -1078,7 +1091,6 @@ class CivicGksClinSigAssertion(
         )
 
         evidence_items: list[CivicGksEvidence] = []
-        eid_links: list[str] = []
         for evidence_item in assertion.evidence_items:
             try:
                 evidence_items.append(CivicGksEvidence(evidence_item))
@@ -1094,9 +1106,6 @@ class CivicGksClinSigAssertion(
                     evidence_item.name,
                     str(e),
                 )
-            finally:
-                # Retain all EID references
-                eid_links.append(f"{LINKS_URL}/evidence/{evidence_item.id}")
 
         if assertion.assertion_type == CivicEvidenceAssertionType.PREDICTIVE:
             evidence_line_cls = TherapeuticEvidenceLine
@@ -1116,7 +1125,6 @@ class CivicGksClinSigAssertion(
                 strengthOfEvidenceProvided=MappableConcept(
                     primaryCoding=(Coding(code=level, system=System.AMP_ASCO_CAP))
                 ),
-                extensions=[Extension(name="citations", value=eid_links)]
             ).root
         ]
 
